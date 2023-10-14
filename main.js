0faf621dc1d02575e93db0632635113b80951dc5
optofer <optofer@gmail.com>
2023-10-14 08:14:20 -0300

    repositorio de la primera entrega

---
+++ main.js
@@ -0,0 +1,39 @@
+let nombre= prompt("ingrese su nombre")
+switch (nombre) {
+    case ("Fernando"):
+        alert("Usuario Registrado")
+var clave= ("as3456")
+    var contraseña=prompt("ingrese su contraseña")
+    if (clave==contraseña) {
+        alert("Bienvenido Fernando")
+    }else{
+        alert("contraseña incorrecta")
+    }
+        break; 
+        
+    case ("Daniel"):
+        alert("Usuario Registrado")
+var clave=("er6582") 
+var contraseña=prompt("ingrese su contraseña")
+    if (clave==contraseña) {
+    alert("Bienvenido Daniel")
+}else{
+    alert("contraseña incorrecta")
+}    
+        break;
+    case ("Carlos"):
+    alert("Usuario Registrado")
+var clave=("32er45") 
+var contraseña=prompt("ingrese su contraseña")
+    if (clave==contraseña) {
+    alert("Bienvenido Carlos")
+}else{
+    alert("contraseña incorrecta")
+} 
+
+        break;
+
+    default:
+        alert("Usuario no registrado")
+        break;
+}
